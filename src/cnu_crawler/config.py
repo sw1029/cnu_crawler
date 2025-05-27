bc07c5837614764f5a506d67b068e35d06ff5f55
sw1029 <yunsw62@gmail.com>
2025-05-28 00:23:22 +0900

    기본구조 완성

--- src/cnu_crawler/config.py
+++ src/cnu_crawler/config.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 import os
 
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parents[2]
 DATA_DIR = BASE_DIR / "data"
 DATA_DIR.mkdir(exist_ok=True)
 
